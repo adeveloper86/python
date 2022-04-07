21aba9916074a07bf87b98a34a24fc2258b579f3
adeveloper86 <adeveloper86@gmail.com>
2022-04-07 11:06:04 +0530

    python

---
+++ helloworld1.py
@@ -0,0 +1,61 @@
+x=1 #int
+y=1.0 #float
+z=1j #complex
+
+#convert from init to float
+a=float(x)
+#float to int
+b=int(y)
+#int to complex
+c=complex(x)
+
+
+print(a)
+print(b)
+print(c)
+
+a=10
+b=20
+
+print(a)
+print(b)
+
+#addition
+x= (a+b)
+
+print(x)
+
+#sub
+x= (a-b)
+
+print(x)
+
+x= (a*b)
+
+print(x)
+
+x= (a/b)
+
+print(x)
+
+x= (a**b)
+
+print(x)
+
+x= (a%b)
+
+print(x)
+
+x= (a//b)
+
+print(x)
+'''
+addition    +
+subtraction -
+multiplication *
+division /
+modulus %
+exponential **
+Floor division //
+'''
+
